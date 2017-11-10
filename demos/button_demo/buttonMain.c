e7d49acd72a21d8655de05ee45c8ca9c4dda3f67
Roman Martinez <ramartinez12@miners.utep.edu>
2017-11-09 18:46:37 -0700

    uploaded samples demos
    Folder that contains the sample files for the msp430 project

---
+++ demos/button_demo/buttonMain.c
@@ -0,0 +1,13 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+
+
+void main(void) 
+{  
+  switch_init();
+  led_init();
+
+  or_sr(0x18);  // CPU off, GIE on
+} 
